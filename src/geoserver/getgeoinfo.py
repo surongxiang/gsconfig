942c5736767ce6f66b8836830a7f4030c9cd2178
wushi <517530615@qq.com>
2015-12-02 14:55:26 +0800

    Create getgeoinfo.py

---
+++ src/geoserver/getgeoinfo.py
@@ -0,0 +1,28 @@
+import pycurl
+import re
+import StringIO
+
+def getGeoinfo(workspaceUrl, user, name, data, GLOBAL_UNAME, GLOBAL_PWD):
+	curl = pycurl.Curl()
+	y = workspaceUrl + user + '/coveragestores/' + name + "/coverages/" + name + ".html"
+	buf = StringIO.StringIO()
+	curl.setopt(pycurl.WRITEFUNCTION, buf.write)
+	curl.setopt(pycurl.URL, y)
+	curl.setopt(pycurl.HTTPHEADER, ['Content-Type:application/json'])
+	curl.setopt(pycurl.USERPWD, GLOBAL_UNAME + ":" + GLOBAL_PWD)
+	curl.perform()
+	thePage = buf.getvalue()
+	reObj1 = re.compile('<li>[\s\S]*?</li>')
+	if thePage[0:2] != '<!':
+		print "Get geoinfo failed." + thePage
+		return
+	geoInfo = reObj1.findall(thePage)[-1].split('[')[-1].split(']')[0].split(',')
+	longitute = geoInfo[0].split(':')
+	latitude = geoInfo[-1].split(':')
+	latitude[0] = latitude[0].strip()
+	latitude[1] = latitude[1].strip()
+	longitute[0] = longitute[0].strip()
+	longitute[1] = longitute[1].strip()
+	newgeoInfo = [longitute[1], latitude[1], longitute[0], latitude[1]]
+	return newgeoInfo
+	buf.close()
